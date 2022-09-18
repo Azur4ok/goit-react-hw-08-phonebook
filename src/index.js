2fa9fd81d4d0c25826ae856e83256caa87053225
Aziz Isanov <alberkrush322@gmail.com>
2022-09-18 16:00:01 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import { App } from './App';
 import { store } from './redux/store';
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router basename="goit-react-hw-08-phonebook">
